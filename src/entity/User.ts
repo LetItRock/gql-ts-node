04be1fd4dc5c51b7f54a067e9268376a4abdf514
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2018-08-05 10:12:33 +0200

    - start server after connected to db; - user registration mutation;

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,7 +1,7 @@
 import { BaseEntity, Entity, Column, PrimaryColumn, BeforeInsert } from 'typeorm'
 import * as uuidv4 from 'uuid/v4'
 
-@Entity()
+@Entity('users')
 export class User extends BaseEntity {
 
   @PrimaryColumn('uuid') id: string
