aeb98a20b8f4ac6ba61e7f63d31bc0ba0e82a678
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2018-08-19 20:05:57 +0200

    - yup schema validation on register resolver; - register resolver test cases;

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -1,12 +1,18 @@
 import { startServer } from '../../startServer';
 import { request } from 'graphql-request';
 import { User } from '../../entity/User';
+import {
+  duplicateEmail,
+  emailNotLongEnough,
+  invalidEmail,
+  passwordNotLongEnough,
+} from './errorMessages';
 
 const email = 'bob@bob.com';
-const password = 'asdfasfa';
-const mutation = `
+const password = 'asadsdasda';
+const mutation = (e: string, p: string) => `
 mutation {
-  register(email: "${email}", password: "${password}") {
+  register(email: "${e}", password: "${p}") {
     path
     message
   }
@@ -22,13 +28,64 @@ beforeAll(async () => {
 });
 
 test('Register user', async () => {
-  const response = await request(getHost(), mutation);
+  // make sure we can register a user
+  const response = await request(getHost(), mutation(email, password));
   expect(response).toEqual({ register: null });
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
   expect(users[0].email).toEqual(email);
   expect(users[0].password).not.toEqual(password);
-  const response2: any = await request(getHost(), mutation);
+
+  // test for duplicate emails
+  const response2: any = await request(getHost(), mutation(email, password));
   expect(response2.register).toHaveLength(1);
-  expect(response2.register[0].path).toEqual('email');
+  expect(response2.register[0]).toEqual({
+    path: 'email',
+    message: duplicateEmail,
+  });
+
+  // catch bad email
+  const response3: any = await request(getHost(), mutation('p', password));
+  expect(response3).toEqual({
+    register: [
+      {
+        path: 'email',
+        message: emailNotLongEnough,
+      },
+      {
+        path: 'email',
+        message: invalidEmail,
+      },
+    ],
+  });
+
+  // catch bad password
+  const response4: any = await request(getHost(), mutation(email, 'a'));
+  expect(response4).toEqual({
+    register: [
+      {
+        path: 'password',
+        message: passwordNotLongEnough,
+      },
+    ],
+  });
+
+  // catch bad email and password
+  const response5: any = await request(getHost(), mutation('e', 'p'));
+  expect(response5).toEqual({
+    register: [
+      {
+        path: 'email',
+        message: emailNotLongEnough,
+      },
+      {
+        path: 'email',
+        message: invalidEmail,
+      },
+      {
+        path: 'password',
+        message: passwordNotLongEnough,
+      },
+    ],
+  });
 });
--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,33 +1,59 @@
 import * as bcrypt from 'bcryptjs';
+import * as yup from 'yup';
+import { User } from '../../entity/User';
 import { ResolverMap } from '../../types/graphql-utils';
 import { GQL } from '../../types/schema';
-import { User } from '../../entity/User';
+import { formatYupError } from '../../utils/formatYupError';
+import {
+  duplicateEmail,
+  emailNotLongEnough,
+  invalidEmail,
+  passwordNotLongEnough,
+} from './errorMessages';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail),
+  password: yup
+    .string()
+    .min(3, passwordNotLongEnough)
+    .max(255),
+});
 
 export const resolvers: ResolverMap = {
   Query: {
     dummy: () => 'dummy!',
   },
   Mutation: {
-    register: async (
-      _,
-      { email, password }: GQL.IRegisterOnMutationArguments
-    ) => {
-      const userAlreadyExist = await User.findOne({ where: { email }, select: ['id']});
+    register: async (_, args: GQL.IRegisterOnMutationArguments) => {
+      try {
+        await schema.validate(args, { abortEarly: false });
+      } catch (err) {
+        return formatYupError(err);
+      }
+      const { email, password } = args;
+      const userAlreadyExist = await User.findOne({
+        where: { email },
+        select: ['id'],
+      });
       if (userAlreadyExist) {
         return [
           {
             path: 'email',
-            message: 'already taken',
+            message: duplicateEmail,
           },
         ];
       }
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = User.create({
         email,
-        password: hashedPassword
+        password: hashedPassword,
       });
       await user.save();
       return null;
-    }
-  }
+    },
+  },
 };
